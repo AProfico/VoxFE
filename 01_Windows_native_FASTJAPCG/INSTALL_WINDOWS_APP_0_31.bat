ceb098542032e38d3de4c5b3ab153e9ae7b598f1
AProfico <54807609+AProfico@users.noreply.github.com>
2026-07-22 09:54:35 +0200

    Delete 01_Windows_native_FASTJAPCG directory

--- 01_Windows_native_FASTJAPCG/INSTALL_WINDOWS_APP_0_31.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0install_windows_app_0_31.ps1"
-echo.
-pause
--- 01_Windows_native_FASTJAPCG/UNINSTALL_WINDOWS_APP_0_31.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0uninstall_windows_app_0_31.ps1"
-echo.
-pause
